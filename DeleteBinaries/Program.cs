338bfb9e974e38b51a6dec59392cf1af3acabc4f
Vaei <jaredtaylor.99@gmail.com>
2023-06-09 20:45:09 +1200

    Added error handling and better logging

--- DeleteBinaries/Program.cs
+++ DeleteBinaries/Program.cs
@@ -1,23 +1,54 @@
-﻿string ProjectDir = Directory.GetCurrentDirectory();
-string PluginsDir = ProjectDir + "\\Plugins";
+﻿/*
+    MIT License
+
+    Copyright (c) 2022-2023 Jared Taylor
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+string ProjectDir = Directory.GetCurrentDirectory();
 
 if (Directory.Exists(ProjectDir))
 {
-    string[] ProjectBinaries = Directory.GetDirectories(ProjectDir, "Binaries", SearchOption.AllDirectories);
-    string[] PluginBinaries = Directory.GetDirectories(PluginsDir, "Binaries", SearchOption.AllDirectories);
+    string[] ProjectDirectories = Directory.GetDirectories(ProjectDir, "Binaries", SearchOption.AllDirectories);
 
-    List<string> Binaries = new List<string>();
-    foreach (string b in ProjectBinaries)
+    List<string> Directories = new List<string>();
+    foreach (string d in ProjectDirectories)
     {
-        Binaries.Add(b);
-    }
-    foreach (string b in PluginBinaries)
-    {
-        Binaries.Add(b);
+        Directories.Add(d);
     }
 
-    foreach (string d in Binaries)
+    int Num = 0;
+    foreach (string d in Directories)
     {
-        Directory.Delete(d, true);
+        Console.WriteLine(d);
+        try
+        {
+            Num++;
+            Directory.Delete(d, true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception occurred when attempting to delete " + d);
+            Console.WriteLine(e.Message);
+        }
     }
+    Console.WriteLine("Deleted " + Num.ToString() + " directories. Press any key to continue...");
+    Console.ReadKey();
 }
